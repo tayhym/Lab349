e787f7960e14f6828132048be27d2cf60b91398c
ktbrennan <ktb807@gmail.com>
2013-11-10 11:42:17 -0500

    Everything working except old problems with read

--- lab3/kernel/C_IRQ_Handler.c
+++ lab3/kernel/C_IRQ_Handler.c
@@ -10,21 +10,19 @@
 #include <arm/timer.h>
 #include <exports.h>
 
-#define RESOLUTION 100
-#define OFFSET 36864
-
 extern volatile unsigned long clock;
 
 /* Called by assembly I_Handler */
 void C_IRQ_Handler() {
-	printf("IRQ!\n");
 	/* Find which interrupt flags are set */
 	uint32_t isTimerInt = reg_read(INT_ICPR_ADDR);
 	/* Check if timer interrupt flag is set */
 	if (((isTimerInt >> INT_OSTMR_0) & 0x01)) {
-		reg_write(OSTMR_OSCR_ADDR,0x0); // should be a clear
+		/* Reset counter */
+		reg_write(OSTMR_OSCR_ADDR,0x0);
+		/* Acknowldge match and clear pending register bit */
 		reg_set(OSTMR_OSSR_ADDR, OSTMR_OIER_E0);
-	
+		/* Increment our personal clock*/
 		clock++;
 	}
 }
--- lab3/tasks/hello/hello.c
+++ lab3/tasks/hello/hello.c
@@ -9,9 +9,17 @@
  */
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdarg.h>
+
+unsigned int test[1];
+
 int main(int argc, char** argv)
 {
 	const char hello[] = "Hello World\r\n";
 	write(STDOUT_FILENO, hello, sizeof(hello) - 1);
+	int i =0;
+	while(1){
+		i++;
+	}
 	return 0x53;
 }
